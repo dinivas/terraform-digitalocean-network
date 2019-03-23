4fb3db07e7dc60f5296e9790369488d0fa8e28b1
Rachid <ouattchidi@gmail.com>
2019-03-24 00:27:15 +0100

    add ids in output

--- examples/simple/outputs.tf
+++ examples/simple/outputs.tf
@@ -1,3 +1,13 @@
+output "network_id" {
+  value       = "${module.network.network_id}"
+  description = "The id of the Network being created"
+}
+
+output "subnet_ids" {
+  value       = "${module.network.subnet_ids}"
+  description = "The id of all subnets being created"
+}
+
 output "network" {
   value       = "${module.network.network}"
   description = "The name of the Network being created"
--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,17 @@
+output "network_id" {
+  value       = "${openstack_networking_network_v2.this.id}"
+  description = "The Network id being created"
+}
+
+output "subnet_ids" {
+  value       = "${data.openstack_networking_subnet_v2.created_subnets.*.id}"
+  description = "Subnets ids being created"
+}
+
 output "network" {
   value       = "${format("%s [id= %s]",openstack_networking_network_v2.this.name, openstack_networking_network_v2.this.id)}"
   description = "The Network being created"
 }
-
 output "subnets" {
   value       = "${formatlist("%s with CIRD %s [id= %s]",data.openstack_networking_subnet_v2.created_subnets.*.name,data.openstack_networking_subnet_v2.created_subnets.*.cidr, data.openstack_networking_subnet_v2.created_subnets.*.id)}"
   description = "The name of all subnets being created"
