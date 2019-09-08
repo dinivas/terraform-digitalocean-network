b53b99aa4b47e3e842bb91bdf5df0f62ee538b5a
Rachid <ouattchidi@gmail.com>
2019-09-08 23:42:42 +0200

    add names to the outputs

--- outputs.tf
+++ outputs.tf
@@ -2,10 +2,19 @@ output "network_id" {
   value       = "${openstack_networking_network_v2.this.id}"
   description = "The Network id being created"
 }
+output "network_name" {
+  value       = "${openstack_networking_network_v2.this.name}"
+  description = "The Network name being created"
+}
 
 output "subnet_ids" {
   value       = "${openstack_networking_subnet_v2.this.*.id}"
   description = "Subnets ids being created"
+}
+
+output "subnet_names" {
+  value       = "${openstack_networking_subnet_v2.this.*.name}"
+  description = "Subnets names being created"
 }
 
 output "network" {
