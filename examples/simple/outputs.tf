176b1cc8b6e82745e19b2d6b86046d675c332113
Rachid <ouattchidi@gmail.com>
2019-09-17 03:43:48 +0200

    minor

--- examples/simple/outputs.tf
+++ examples/simple/outputs.tf
@@ -3,8 +3,18 @@ output "network_id" {
   description = "The id of the Network being created"
 }
 
+output "network_name" {
+  value       = "${module.network.network_name}"
+  description = "The name of the Network being created"
+}
+
 output "subnet_ids" {
   value       = "${module.network.subnet_ids}"
+  description = "The id of all subnets being created"
+}
+
+output "subnet_names" {
+  value       = "${module.network.subnet_names}"
   description = "The id of all subnets being created"
 }
 
